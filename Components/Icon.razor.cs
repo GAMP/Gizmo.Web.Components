7ea7aaebeb62eafa11cb0ac38eb640fb8628e1f6
klapsis-netprojects <klapsis@netprojects.gr>
2021-04-07 17:47:47 +0300

    Icon component added.

---
+++ Components/Icon.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Components
+{
+    public partial class Icon : CustomDOMComponentBase
+    {
+        public enum IconBackgroundStyles
+        {
+            None,
+            Circle,
+            Square
+        }
+
+        #region CONSTRUCTOR
+        public Icon()
+        {
+            ClassMapper
+                .If("fa-stack", () => BackgroundStyle != IconBackgroundStyles.None);
+
+            IconClassMapper
+                .If("fa-stack-1x", () => BackgroundStyle != IconBackgroundStyles.None);
+        }
+        #endregion
+
+        private ClassMapper _iconClassMapper;
+
+        protected ClassMapper IconClassMapper
+        {
+            get
+            {
+                if (_iconClassMapper == null)
+                    _iconClassMapper = new ClassMapper();
+
+                return _iconClassMapper;
+            }
+        }
+
+        [Parameter]
+        public string Source { get; set; }
+
+        [Parameter]
+        public string Size { get; set; }
+
+        [Parameter]
+        public string Color { get; set; }
+
+        [Parameter]
+        public IconBackgroundStyles BackgroundStyle { get; set; } = IconBackgroundStyles.None;
+
+        [Parameter]
+        public string BackgroundColor { get; set; }
+    }
+}
