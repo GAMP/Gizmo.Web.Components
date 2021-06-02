b0644ffcc647412a31bc1f4321e9fc9cb1ff5ffb
klapsis-netprojects <klapsis@netprojects.gr>
2021-06-02 13:29:36 +0300

    Add Badge component.

---
+++ Components/Badge.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Gizmo.Web.Components
+{
+    public partial class Badge : CustomDOMComponentBase
+    {
+        public enum BadgeSize
+        {
+            Small = 0,
+            Normal = 1
+        }
+
+        #region CONSTRUCTOR
+        public Badge()
+        {
+        }
+        #endregion
+
+        #region PROPERTIES
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public bool IsInline { get; set; }
+
+        [Parameter]
+        public BadgeSize Size { get; set; } = BadgeSize.Normal;
+
+        [Parameter]
+        public string Color { get; set; }
+
+        [Parameter]
+        public string BackgroundColor { get; set; } = "#5a67f2";
+
+        #endregion
+
+        protected string ClassName => new ClassMapper()
+                 .Add("g-badge")
+                 .If("corner", () => !IsInline)
+                 .If("small", () => Size == BadgeSize.Small)
+                 .AsString();
+    }
+}
