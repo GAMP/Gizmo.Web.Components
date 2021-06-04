79cd27b8fdc66c524d9f7e574fb747becfffb62d
klapsis-netprojects <klapsis@netprojects.gr>
2021-06-04 16:49:41 +0300

    Add switch component.

---
+++ Components/Switch.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Components
+{
+    public partial class Switch : CustomDOMComponentBase
+    {
+        #region CONSTRUCTOR
+        public Switch()
+        {
+        }
+        #endregion
+
+        #region FIELDS
+        private bool _isChecked;
+        #endregion
+
+        #region PROPERTIES
+
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public bool IsChecked
+        {
+            get { return _isChecked; }
+            set { _isChecked = value; }
+        }
+
+        [Parameter]
+        public EventCallback<bool> IsCheckedChanged
+        {
+            get; set;
+        }
+
+        [Parameter]
+        public bool IsDisabled
+        {
+            get; set;
+        }
+
+        #endregion
+
+        protected Task OnChangeHandler(ChangeEventArgs args)
+        {
+            IsChecked = (bool)args.Value;
+            return IsCheckedChanged.InvokeAsync(IsChecked);
+        }
+    }
+}
