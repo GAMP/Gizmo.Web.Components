d053be70eb03cd1bdb80839fc6a3299be0583d65
klapsis-netprojects <72749987+klapsis-netprojects@users.noreply.github.com>
2021-06-22 10:30:22 +0300

    Add progress bars.

---
+++ Components/PercentageStackBar.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Gizmo.Web.Components
+{
+    public partial class PercentageStackBar
+    {
+        [Parameter]
+        public List<decimal> Values { get; set; }
+
+        [Parameter]
+        public List<string> Colors { get; set; }
+
+        protected string ClassName => new ClassMapper()
+                 .Add("giz-percentage-stack-bar")
+                 .AsString();
+
+    }
+}
