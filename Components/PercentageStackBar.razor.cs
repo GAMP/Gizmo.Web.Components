5530b2162d4f6fe726bf86afac61e6780ba73a68
klapsis-netprojects <72749987+klapsis-netprojects@users.noreply.github.com>
2021-06-23 16:29:40 +0300

    Update PercentageStackBar.

--- Components/PercentageStackBar.razor.cs
+++ Components/PercentageStackBar.razor.cs
@@ -1,18 +1,50 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Gizmo.Web.Components
 {
     public partial class PercentageStackBar
     {
+
+
+        private List<decimal> _values;
+        private decimal _total;
+        private List<decimal> _percentValues = new List<decimal>();
+
         [Parameter]
-        public List<decimal> Values { get; set; }
+        public List<decimal> Values
+        {
+            get
+            {
+                return _values;
+            }
+            set
+            {
+                _values = value;
+
+                if (_values != null)
+                {
+                    _total = _values.Sum(a => a);
+
+                    _percentValues.Clear();
+                    foreach (var val in _values)
+                    {
+                        _percentValues.Add(val / _total * 100);
+                    }
+                }
+                else
+                {
+                    _total = 0;
+                }
+            }
+        }
 
         [Parameter]
         public List<string> Colors { get; set; }
 
         protected string ClassName => new ClassMapper()
-                 .Add("giz-percentage-stack-bar")
+                 .Add("stacked-progress")
                  .AsString();
 
     }
