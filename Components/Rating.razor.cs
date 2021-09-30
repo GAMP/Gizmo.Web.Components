5ad3924d961f68f0762d7e3d64ca7f3e7df88575
klapsis-netprojects <72749987+klapsis-netprojects@users.noreply.github.com>
2021-09-30 13:00:58 +0300

    Fix rating icon and binding.

--- Components/Rating.razor.cs
+++ Components/Rating.razor.cs
@@ -4,8 +4,28 @@ namespace Gizmo.Web.Components
 {
     public partial class Rating
     {
+        private decimal _value;
+        private int _selectedStar = 0;
+
         [Parameter]
-        public decimal Value { get; set; }
+        public decimal Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (_value == value)
+                    return;
+
+                _value = value;
+                ValueChanged.InvokeAsync(_value);
+            }
+        }
+
+        [Parameter]
+        public EventCallback<decimal> ValueChanged { get; set; }
 
         [Parameter]
         public string BackgroundColor { get; set; } = "#585d6f";
@@ -13,10 +33,29 @@ namespace Gizmo.Web.Components
         [Parameter]
         public string Color { get; set; } = "#ffffff";
 
+        [Parameter]
+        public bool IsReadOnly { get; set; }
+
+        protected void OnMouseOverStar(int index)
+        {
+            _selectedStar = index;
+        }
+
+        protected void OnMouseOut()
+        {
+            _selectedStar = 0;
+        }
+
+        protected void OnClickStar(int index)
+        {
+            Value = index;
+        }
+
         #region CLASSMAPPERS
 
         protected string ClassName => new ClassMapper()
                  .Add("giz-rating")
+                 .If("readonly", () => IsReadOnly)
                  .AsString();
 
         #endregion
