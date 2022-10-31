c55f0377ef85834fc989bd956055be5938a50b1b
klapsis-netprojects <klapsis@netprojects.gr>
2022-10-31 17:04:42 +0200

    Add file input component.

---
+++ Components/FileInput.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Components
+{
+    public partial class FileInput : CustomDOMComponentBase
+    {
+        #region PROPERTIES
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public EventCallback<InputFileChangeEventArgs> OnChange { get; set; }
+
+        #endregion
+
+        private Task OnInputFileChange(InputFileChangeEventArgs e)
+        {
+            return OnChange.InvokeAsync(e);
+        }
+    }
+}
