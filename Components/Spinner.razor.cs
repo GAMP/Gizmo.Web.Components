b1036690ebb70b26bcc2c3942b325bc150783071
klapsis-netprojects <klapsis@netprojects.gr>
2023-03-31 16:51:40 +0300

    Fix.

--- Components/Spinner.razor.cs
+++ Components/Spinner.razor.cs
@@ -1,6 +1,13 @@
-﻿namespace Gizmo.Web.Components.Components
+﻿namespace Gizmo.Web.Components
 {
     public partial class Spinner : CustomDOMComponentBase
     {
+        #region CLASSMAPPERS
+
+        protected string ClassName => new ClassMapper()
+                 .Add("giz-animate-spinner")
+                 .AsString();
+
+        #endregion
     }
 }
