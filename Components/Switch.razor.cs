3f7d912433606505b58c290124ca61a50e00acb6
Andrey Pestunov <masterlifting@gmail.com>
2023-07-31 15:18:19 +0200

    fead: Add switch component

--- Components/Switch.razor.cs
+++ Components/Switch.razor.cs
@@ -1,46 +1,16 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Gizmo.Web.Components.Extensions;
+using Microsoft.AspNetCore.Components;
 using System.Threading.Tasks;
 
 namespace Gizmo.Web.Components
 {
     public partial class Switch : CustomDOMComponentBase
     {
-        #region CONSTRUCTOR
-        public Switch()
-        {
-        }
-        #endregion
-
-        #region FIELDS
-
-        private bool _isChecked;
-
-        #endregion
-
-        #region PROPERTIES
-
-        [Parameter]
-        public string Label { get; set; }
-
-        [Parameter]
-        public bool IsChecked
-        {
-            get { return _isChecked; }
-            set { _isChecked = value; }
-        }
-
-        [Parameter]
-        public EventCallback<bool> IsCheckedChanged { get; set; }
-
-        [Parameter]
-        public bool IsDisabled { get; set; }
-
-        [Parameter]
-        public Icons? ThumbSVGIcon { get; set; }
-
-        #endregion
-
-        #region EVENTS
+        [Parameter] public string Label { get; set; }
+        [Parameter] public bool IsChecked { get; set; }
+        [Parameter] public bool IsDisabled { get; set; }
+        [Parameter] public EventCallback<bool> IsCheckedChanged { get; set; }
+        [Parameter] public IconSizes Size { get; set; } = IconSizes.Medium;
 
         protected Task OnChangeHandler(ChangeEventArgs args)
         {
@@ -50,16 +20,10 @@ namespace Gizmo.Web.Components
             IsChecked = (bool)args.Value;
             return IsCheckedChanged.InvokeAsync(IsChecked);
         }
-
-        #endregion
-
-        #region CLASSMAPPERS
-
         protected string ClassName => new ClassMapper()
-                 .Add("giz-switch")
-                 .If("disabled", () => IsDisabled)
-                 .AsString();
-
-        #endregion
+            .Add("giz-switch")
+            .Add($"giz-switch--{Size.ToDescriptionString()}")
+            .If("disabled", () => IsDisabled)
+            .AsString();
     }
 }
